7b1a1018bbcd8d18debc9831b1a9ca6375bd66e0
Knockyou <ssm02086@gmail.com>
2025-06-27 16:41:42 +0900

    1. 업데이트 로직 추가  - 머스테치 추가  - 컨트롤러(페이지 get, 데이터 변겨 버튼 함수 추가) 2. 쿼리 더미 데이터 추가  - 프로퍼티스 추가 3.  ui 버튼 스타일 추가

--- src/main/java/com/example/knockProject/controller/ArticleController.java
+++ src/main/java/com/example/knockProject/controller/ArticleController.java
@@ -84,6 +84,44 @@ public class ArticleController {
         return "articles/ImageShow";
     }
 
+    // 상세 페이지에서 수정 페이지 오픈
+    @GetMapping("/article/update/{number}")
+    public String articleUpdate(@PathVariable Long number, Model model){
+
+        //PathVariable 요청 변수 수집
+        log.info("number : " + number);
+
+        //게시글 번호 확인해서 view 처리
+        // 받아온 아이디 값을 db로 조회
+        //Optional<Article> saved = articleRepository.findById(number);
+        // 조회시 없을 떄 null을 리턴 한다는 것
+        Article saved = articleRepository.findById(number).orElse(null);
+
+        // view 화면에 보여주기
+        // addAtrb 활용하여 모델에 데이터 등록
+        model.addAttribute("article", saved);
+
+        return "articles/update";
+    }
+
+    // 수정 완료 버튼
+    @PostMapping("/article/update/submit")
+    public String articleUpdateSubmit(ArticleForm articleForm){
+        //System.out.println("1.확인용 : " + articleForm.toString());
+        // 로그 작성
+        log.info(articleForm.toString());
+        // DTO -> Entity
+        Article article = articleForm.toEntity();
+        //System.out.println("2.확인용 Article : " + article.toString());
+        log.info(articleForm.toString());
+        // Repo -> DB save
+        article.SetId(number);
+        Article saved = articleRepository.save(article);
+        //System.out.println("3.확인용 Article save : " + saved.toString());
+        log.info(saved.toString());
+        return "redirect:/article/"+saved.getId(); // 리다이렉트 -> redirect:주소
+    }
+
 
 
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO article VALUES(1, '동해물과 백두산이','김삿갓');
+INSERT INTO article VALUES(2, '마르고 닭도록','이몽룡');
+INSERT INTO article VALUES(3, '하느님이 보우하사','선녀');
+INSERT INTO article VALUES(4, '우리나라 만세','나뭇꾼');
